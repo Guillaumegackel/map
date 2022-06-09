acd5b938b5cc086bfa94520ecbf74305841da8b4
Webme <guillaume.gackel@gmail.com>
2022-06-09 16:03:42 +0200

    axios lat lng

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 
-// import { getPlacesdata } from "./api";
+import { getPlacesdata } from "./api";
 
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
@@ -9,14 +9,23 @@ import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 import Map from "./components/Map/Map";
 
 const App = () => {
-//   const [places, setPlaces] = useState([]);
+  const [places, setPlaces] = useState([]);
 
-//   useEffect(() => {
-//     getPlacesdata().then((data) => {
-//       console.log(data);
-//       setPlaces(data);
-//     });
-//   }, []);
+  const [coordinates, setCoordinates] = useState({lat:0, lng:0});
+  const [bounds, setBounds]= useState("");
+
+  useEffect(()=>{
+navigator.geolocation.getCurrentPosition(({coordinates: {latitude, longitude}})=>{
+  setCoordinates({ lat:latitude, lng:longitude });
+});
+  },[])
+
+  useEffect(() => {
+    getPlacesdata( bounds.sw, bounds.ne)
+    .then((data) => {
+      setPlaces(data);
+    });
+  }, [coordinates, bounds]);
   return (
     <>
       <CssBaseline />
@@ -26,7 +35,11 @@ const App = () => {
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map 
+          setCoordinates={setCoordinates}
+          setBounds={setBounds}
+          coordinates={coordinates}
+          />
         </Grid>
       </Grid>
       <PlaceDetails />
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab/Rating';
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
 
-import useStyles from './styles.js';
+import useStyles from "./styles.js";
 
-const Map = () => {
-	const classes = useStyles();
-	const isMobile= useMediaQuery('(min-width:600px)');
-
-	const coordinates = {lat:0, lng:0};
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
 
   return (
-<div className={classes.mapContainer}>
-	<GoogleMapReact 
-	bootstrapURLKeys={{ key: 'AIzaSyDUH4An6-v33Qm1MLzlEkTfmUSNBkujTE0' }}
-	defaultCenter={coordinates}
-	center={coordinates}
-	defaultZoom={7}
-	margin={[50,50,50,50]}
-	options={''}
-	onChange={''}
-	onChildClick={''}
-	>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDUH4An6-v33Qm1MLzlEkTfmUSNBkujTE0" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={(e) => {
+          console.log(e);
 
-	</GoogleMapReact>
-</div>  )
-}
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 
-export default Map
+export default Map;
